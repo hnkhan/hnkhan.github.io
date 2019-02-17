be2b943d2076ca500bf0b7270ca4026bc0901fc0
Hussain Khan <hnkhan@ualberta.ca>
2019-02-17 00:30:06 +0000

    Deploy onto github pages

---
+++ precache-manifest.10d4a8044bb5f69b6617e3493de2a6dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8e52c098feacdb6f211b",
+    "url": "/static/js/main.8e52c098.chunk.js"
+  },
+  {
+    "revision": "8d44809b9ff06ba7d425",
+    "url": "/static/js/1.8d44809b.chunk.js"
+  },
+  {
+    "revision": "8e52c098feacdb6f211b",
+    "url": "/static/css/main.5c3e82f2.chunk.css"
+  },
+  {
+    "revision": "bd00f122fdeab4ddf925cf27766efe5a",
+    "url": "/index.html"
+  }
+];
